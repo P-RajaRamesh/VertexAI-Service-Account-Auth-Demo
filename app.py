85d62b342af827c929407fa4a010f759ce421e23
Raja Ramesh <rajaramesh7410@gmail.com>
2025-09-07 19:16:10 +0530

    commit

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
+import tempfile
+import base64
+
 load_dotenv()
+
+# Decode your Base64 key
+encoded_key = os.getenv("SERVICE_ACCOUNT_KEY_BASE64")
+decoded_bytes = base64.b64decode(encoded_key)
+
+# Write to a temporary file
+with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp_file:
+    temp_file.write(decoded_bytes)
+    temp_file_path = temp_file.name
 
 st.set_page_config(
     page_title="Demo",
@@ -14,15 +26,19 @@ st.set_page_config(
 
 st.title("Demo App")
 
-SERVICE_ACCOUNT_KEY_FILE=os.getenv("SERVICE_ACCOUNT_KEY_FILE")
+# SERVICE_ACCOUNT_KEY_FILE=os.getenv("SERVICE_ACCOUNT_KEY_FILE")
 PROJECT_ID=os.getenv("PROJECT_ID")
 LOCATION=os.getenv("LOCATION")
 MODEL_NAME=os.getenv("MODEL_NAME")
 
 credentials=service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_KEY_FILE,
+    # SERVICE_ACCOUNT_KEY_FILE,
+    temp_file_path,
     scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
+
+# Deleting temp file after authentication from Temp folder
+os.remove(temp_file_path)
 
 client = genai.Client(
     vertexai=True, 
